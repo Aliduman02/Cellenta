a65a11b7b18cf922ca94902a18ebc4ca6adbe826
mahmoudd202 <mahmoudd202@hotmail.com>
2025-07-21 11:48:40 +0300

    Some changes were made to the classes

--- Hazelcast/src/main/java/com/i2i/cellenta/hazelcast/util/MsisdnGenerator.java
+++ Hazelcast/src/main/java/com/i2i/cellenta/hazelcast/util/MsisdnGenerator.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class MsisdnGenerator {
     private static final Random RANDOM = new Random();
     public static String generateRandomMsisdn() {
-        long num = Math.abs(RANDOM.nextLong() % 1_000_000_000L);
-        return "05" + String.format("%09d", num);
+        long num = Math.abs(RANDOM.nextLong() % 100_000_000L);
+        return "05" + String.format("%08d", num);
     }
 }
